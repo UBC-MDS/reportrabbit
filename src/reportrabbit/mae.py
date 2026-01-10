3e87f0eebfda98beca3769265b896575d35470f4
Raghav Gupta <raghav9048@gmail.com>
2026-01-09 22:34:15 -0800

    updated get_mae function

--- src/reportrabbit/mae.py
+++ src/reportrabbit/mae.py
@@ -5,6 +5,7 @@ A module that calculates the Mean Absolute Error (MAE).
 def get_mae(y_true, y_pred):
     """
     Calculates the Mean Absolute Error (MAE) and returns the result.
+    
     MAE measures the average absolute difference between the
     observed values and the predicted values.
 
